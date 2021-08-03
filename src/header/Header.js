564d64e19cc121834cded9d08a2c666fed54be76
Parthvsquare <parthvsquare@gmail.com>
2021-08-04 01:23:41 +0530

    added header icons

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOptions from './components/HeaderOptions';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccount from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import ChatIcon from '@material-ui/icons/Chat'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+
 function Header() {
     return (
         <div className='header'>
@@ -12,6 +19,11 @@ function Header() {
                 </div>
             </div>
             <div className="header__right">
+                <HeaderOptions Icon={HomeIcon} title='Home' />
+                <HeaderOptions Icon={SupervisorAccount} title='My Network' />
+                <HeaderOptions Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOptions Icon={ChatIcon} title='Messaging' />
+                <HeaderOptions Icon={NotificationsIcon} title='Notifications' /> 
             </div>
         </div>
     )
