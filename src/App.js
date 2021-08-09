a67499e640bc12709bf155cd1acf40dbc1751e5a
Parthvsquare <parthvsquare@gmail.com>
2021-08-09 21:45:08 +0530

    added redux for login and log out

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import './App.css';
 import Header from './header/Header';
 import SideBar from './sideBar/SideBar'
 import Feed from './feed/Feed'
+import { selectUser } from './features/userSlice';
+import Login from "./login/Login"
 function App() {
+  const user = useSelector(selectUser)
   return (
     <div className="app">
         <Header />
-        <div className="app__body">
+        {!user ?
+        (
+          <Login />
+        )
+       : (<div className="app__body">
           <SideBar />
           <Feed />
         </div>
+       )
+        }
     </div>
   );
 }
