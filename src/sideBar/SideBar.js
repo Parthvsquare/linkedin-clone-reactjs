d288f3d5214793bbd0d0e06fc6a11f683c664687
Parthvsquare <parthvsquare@gmail.com>
2021-08-09 23:17:52 +0530

    dynamic sidebar profile

--- src/sideBar/SideBar.js
+++ src/sideBar/SideBar.js
@@ -1,8 +1,10 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
+import { selectUser } from '../features/userSlice'
+import {useSelector} from 'react-redux'
 import './SideBar.css'
 function SideBar() {
-
+ const user = useSelector(selectUser);
     const recentItem = (topic) => (
         <div className="sidebar__recentItem">
             <span className="sidebar__hash">#</span>
@@ -13,9 +15,11 @@ function SideBar() {
         <div className='sidebar'>
            <div className="sidebar__top">
                <img src="https://images.unsplash.com/photo-1628089928275-55335514dd53?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80" alt="" />
-               <Avatar className='sidebar__avatar' />
-               <h2>Parth Vsquare</h2>
-               <h4>parthvsquare@gmail.com</h4>
+               <Avatar className='sidebar__avatar'>
+                   {user.email[0]}
+                   </Avatar>
+               <h2>{user.displayName}</h2>
+               <h4> {user.email}</h4>
                </div> 
                <div className="sidebar__stats">
                    <div className="sidebar__stat">
